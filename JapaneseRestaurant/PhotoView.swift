4f51cae07fc9b82258f736ac37b587eef5602535
joaobug <joaoenfrance@gmail.com>
2024-01-26 20:43:25 -0300

    App conclusion on the 26th of January A restaurant app with 3 tabs Practice using tabview

---
+++ JapaneseRestaurant/PhotoView.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoView.swift
+//  JapaneseRestaurant
+//
+//  Created by Joao Leal on 26/01/2024.
+//
+
+import SwiftUI
+
+struct PhotoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhotoView()
+}
