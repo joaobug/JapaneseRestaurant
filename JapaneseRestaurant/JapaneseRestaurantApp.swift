1cc77408e2e75a34cb3f5740a718655cdbfe8122
joaobug <joaoenfrance@gmail.com>
2024-01-26 11:24:17 -0300

    Initial Commit

---
+++ JapaneseRestaurant/JapaneseRestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  JapaneseRestaurantApp.swift
+//  JapaneseRestaurant
+//
+//  Created by Joao Leal on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct JapaneseRestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
